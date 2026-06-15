cb9259e9c391642eb7e8528ca825e4e233de46ee
lamlt0607 <lamlt0607@gmail.com>
2026-06-15 13:12:30 +0700

    Adjust QR code positioning and dimensions

--- server.js
+++ server.js
@@ -72,10 +72,10 @@ app.get('*', async (c) => {
         const frameHeight = 800;
 
         // 1. Tọa độ của vùng mã QR 
-        const destX = 105;       // Đẩy mạnh sang trái (Cũ là 140)
-        const destY = 50;        // Đẩy lên trên (Cũ là 80)
-        const destWidth = 310;   // Thu nhỏ một chút để tạo lề trắng bọc quanh viền (Cũ là 320)
-        const destHeight = 310;
+        const destX = 160;       // Đẩy mạnh sang trái (Cũ là 140)
+        const destY = 100;        // Đẩy lên trên (Cũ là 80)
+        const destWidth = 320;   // Thu nhỏ một chút để tạo lề trắng bọc quanh viền (Cũ là 320)
+        const destHeight = 320;
 
         // 2. Tọa độ chữ (Đang hiển thị rất đẹp nên giữ nguyên)
         let textStartY = 590;    
